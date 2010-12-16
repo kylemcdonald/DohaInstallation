45ce58181da418bae296c308b0d223529e1bc068
Kyle McDonald <kyle@kylemcdonald.net>
2010-12-16 18:05:13 +0300

    ok this works

--- scripts/complete-restart.sh
+++ scripts/complete-restart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./home/kyle/Desktop/openFrameworks/apps/DohaInstallation/scripts/restart.sh
+cd /home/kyle/Desktop/openFrameworks/apps/DohaInstallation/scripts
+./restart.sh
 sleep 30
 shutdown -r now
