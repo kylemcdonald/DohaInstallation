3ae26c08a39fdbc9a9729c2878dea090cf1b6f55
Kyle McDonald <kyle@kylemcdonald.net>
2011-04-12 11:42:41 +0300

    added shawn's tracking code

---
+++ KinectShawn/src/testApp.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+#include "ofxOsc.h"
+#include "ofxVectorMath.h"
+#include "ofxXmlSettings.h"
+
+class testApp : public ofBaseApp {
+	public:
+
+		void setup();
+		void setupOsc();
+		void update();
+		void draw();
+		void exit();
+	
+		void drawPointCloud();
+
+		void keyPressed  (int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+		ofxKinect kinect;
+
+		ofxCvColorImage		colorImg;
+
+		ofxCvGrayscaleImage 	grayImage;
+		ofxCvGrayscaleImage 	grayThresh;
+		ofxCvGrayscaleImage 	grayThreshFar;
+
+		ofxCvContourFinder 	contourFinder;
+		
+		bool				bThreshWithOpenCV;
+		bool				drawPC;
+
+		int 				nearThreshold;
+		int					farThreshold;
+
+		int					angle;
+		
+		int 				pointCloudRotationY;
+	
+		int					blobSize;
+	
+		ofxVec3f			curPoint;
+		ofxOscSender		oscSender;
+	
+		ofxXmlSettings		imageSettings;
+};
