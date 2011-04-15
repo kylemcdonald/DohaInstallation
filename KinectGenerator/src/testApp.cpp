5471cd3428956c42320d83dd9b29653dbe8c6258
Kyle McDonald <kyle@kylemcdonald.net>
2011-04-15 18:17:20 +0300

    added smoothing, seems to help a little bit

--- KinectGenerator/src/testApp.cpp
+++ KinectGenerator/src/testApp.cpp
@@ -3,16 +3,17 @@
 //--------------------------------------------------------------
 void testApp::setup() {
 	ofSetVerticalSync(true);
-	
+	ofSetFrameRate(60);
+
 	panel.setup("Control Panel", 5, 5, 280, 600);
 	panel.addPanel("Contours");
 	panel.addSlider("alpha", "alpha", .05, 0, 1);
-	panel.addSlider("blurAmount", "blurAmount", 4, 0, 10, true);
+	panel.addSlider("blurAmount", "blurAmount", 8, 0, 16, true);
 	panel.addSlider("threshLevel", "threshLevel", 64, 0, 255, true);
 	panel.addSlider("minArea", "minArea", 200, 0, 100 * 100, true);
 	panel.addSlider("maxArea", "maxArea", 10000, 32 * 32, 240 * 240, true);
 	panel.addSlider("nConsidered", "nConsidered", 8, 1, 16, true);
-	
+
 	panel.addPanel("Camera");
 	panel.addSlider("maxLen", "maxLen", 8, 0, 20);
 	panel.addSlider("stepSize", "stepSize", 1, 1, 10, true);
@@ -25,12 +26,12 @@ void testApp::setup() {
 	panel.addSlider("camrx", "camrx", 20.2, -180, 180);
 	panel.addSlider("camry", "camry", 0, -180, 180);
 	panel.addSlider("camrz", "camrz", 0, -180, 180);
-	
+
 	cam.setup();
-	
+
 	blur.allocate(640, 480);
 	thresh.allocate(640, 480);
-	
+
 	setupOsc();
 }
 
@@ -41,7 +42,7 @@ void testApp::setupOsc() {
 	}
 	string address = settings.getValue("address", "255.255.255.255");
 	int port = settings.getValue("port", 8888);
-	
+
 	cout << "Broadcasting to " << address << ":" << port << endl;
 	oscSender.setup(address, port);
 }
@@ -56,24 +57,55 @@ IplImage* toCv(unsigned char* pixels, int width, int height, int type) {
 void testApp::updateOsc() {
 	ofxOscMessage message;
 	message.setAddress("mouse");
-	
+
 	vector<ofxCvBlob>& blobs = finder.blobs;
 	for(int i = 0; i < blobs.size(); i++) {
 		ofxVec2f curPoint(blobs[i].centroid);
 		// squeeze because the floor has a different aspect ratio than the camera
 		curPoint.x = ofMap(curPoint.x, 0, 640, .2, .8);
 		curPoint.y = ofMap(curPoint.y, 0, 480, 0, 1);
-	
+ 
 		//project forward
 		message.addFloatArg(curPoint.x);
 		message.addFloatArg(.8);
-		
+ 
 		// project upward
 		message.addFloatArg(curPoint.x);
 		message.addFloatArg(ofMap(curPoint.y, 0, 1, 0, .6));
 	}
-	
+ 
 	oscSender.sendMessage(message);
+}
+
+vector<ofPoint> ofGetSmoothed(const vector<ofPoint>& polyline, int smoothingSize, float smoothingShape = 0) {
+	vector<ofPoint> result = polyline;
+ 
+// precompute weights and normalization
+	vector<float> weights;
+	float weightSum = 0;
+	weights.push_back(1); // center weight
+// side weights
+	for(int i = 1; i <= smoothingSize; i++) {
+		float curWeight = ofMap(i, 0, smoothingSize, 1, smoothingShape);
+		weights.push_back(curWeight);
+		weightSum += curWeight;
+	}
+	float weightNormalization = 1 / (1 + 2 * weightSum);
+ 
+// use weights to make weighted averages of neighbors
+	int n = polyline.size();
+	for(int i = 0; i < n; i++) {
+		for(int j = 1; j <= smoothingSize; j++) {
+			int leftPosition = (n + i - j) % n;
+			int rightPosition = (i + j) % n;
+			const ofPoint& left = polyline[leftPosition];
+			const ofPoint& right = polyline[rightPosition];
+			result[i] += (left + right) * weights[j];
+		}
+		result[i] *= weightNormalization;
+	}
+ 
+	return result;
 }
 
 //--------------------------------------------------------------
@@ -84,13 +116,13 @@ void testApp::update() {
 	cam.setOrthoScale(panel.getValueF("orthoScale"));
 	cam.setPosition(ofxVec3f(panel.getValueF("camx"), panel.getValueF("camy"), panel.getValueF("camz")));
 	cam.setRotation(ofxVec3f(panel.getValueF("camrx"), panel.getValueF("camry"), panel.getValueF("camrz")));
-	
+ 
 	int blurAmount = panel.getValueI("blurAmount");
 	int threshLevel = panel.getValueI("threshLevel");
 	int minArea = panel.getValueI("minArea");
 	int maxArea = panel.getValueI("maxArea");
 	int nConsidered = panel.getValueI("nConsidered");
-	
+ 
 	cam.update();
 	if(cam.isFrameNew()) {
 		float alpha = panel.getValueF("alpha");
@@ -99,17 +131,20 @@ void testApp::update() {
 		cvAddWeighted(camIpl, alpha, blur.getCvImage(), beta, 0, blur.getCvImage());
 		blur.flagImageChanged();
 		blur.blur(blurAmount * 2 + 1);
-		
+ 
 		thresh = blur;
 		thresh.threshold(threshLevel);
 		finder.findContours(thresh, minArea, maxArea, nConsidered, false, false);
-		
+ 
 		// make the average the centroid
 		// should be more stable than the moments
 		vector<ofxCvBlob>& blobs = finder.blobs;
 		for(int i = 0; i < blobs.size(); i++) {
 			ofxCvBlob& cur = blobs[i];
 			vector<ofPoint>& pts = cur.pts;
+			
+			pts = ofGetSmoothed(pts, 8);
+			
 			ofPoint& centroid = cur.centroid;
 			centroid.set(0, 0);
 			for(int j = 0; j < pts.size(); j++) {
@@ -117,37 +152,38 @@ void testApp::update() {
 			}
 			centroid /= pts.size();
 		}
-		
+ 
 		updateOsc();
 	}
 }
-
+ 
 //--------------------------------------------------------------
 void testApp::draw() {
 	ofBackground(0, 0, 0);
 	ofSetColor(255, 255, 255);
 	cam.draw(0, 0);
-	
+ 
 	blur.draw(640, 0);
 	ofEnableAlphaBlending();
 	ofSetColor(255, 0, 0, 128);
 	thresh.draw(640, 0);
 	ofDisableAlphaBlending();
-	
+ 
 	finder.draw(0, 0);
-	
+ 
 	vector<ofxCvBlob>& blobs = finder.blobs;
 	for(int i = 0; i < blobs.size(); i++) {
 		ofPoint& cur = blobs[i].centroid;
 		ofCircle(cur.x, cur.y, 5);
 	}
-	
+ 
 	ofSetColor(255, 0, 0);
 	ofLine(320, 0, 320, 480);
 	ofLine(0, 240, 640, 240);
 	ofNoFill();
 	ofRect(0, 0, 640, 480);
 }
-
+ 
 void testApp::keyPressed(int key) {
 }
+ 
